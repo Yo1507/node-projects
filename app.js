3f00995aee8885bf732248dc377e83c91ff0ecac
Yo <yoan.denis@live.fr>
2017-08-26 20:06:20 +0200

    add nodemon

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
 const notes = require('./notes.js');
-
-var filteredArray = _.uniq(['Yo', 1, 'Yo', 1, 2, 3, 4]);
-console.log(filteredArray);
-// console.log('Result', notes.add(1, 2));
-
-// var user = os.userInfo();
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
